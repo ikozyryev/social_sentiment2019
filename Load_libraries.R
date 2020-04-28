60ee371272684aeb875cad8cb6e5972928f13588
ikozyryev <ikozyryev@gmail.com>
2020-04-28 15:38:11 -0400

    Add files via upload

---
+++ Load_libraries.R
@@ -0,0 +1,19 @@
+library(rtweet)
+library(httpuv)
+library(dplyr)
+library(reshape)
+library(ggplot2)
+library(readr)
+library(qdapRegex) # for removing URLs from tweet text
+library(tm) # load the text mining package
+library(tidytext)
+library(tidyr)
+library(dplyr)
+library(textdata) # to load various text datasets for sentiment analysis
+library(reshape2)
+library(wordcloud)
+library(stats)
+library(caret)
+# library(rlang)
+#install.packages("tidyverse")
+# library(qdap) # to extract term frequency
